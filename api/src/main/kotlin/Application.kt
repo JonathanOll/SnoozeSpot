0f82ab9d9a5fc610f844b86113f759b1533d9d88
p2218005 <jonathan.ollivier@etu.univ-lyon1.Fr>
2025-09-11 12:07:53 +0200

    ajout api

---
+++ api/src/main/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package iut.fauryollivier.snoozespot
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
