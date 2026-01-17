8e763fd1229dc90bbf727451041a59950361b881
Jonathan <70845195+JonathanOll@users.noreply.github.com>
2026-01-17 18:05:28 +0100

    Room post cache

--- app/app/src/main/java/iut/fauryollivier/snoozespot/room/AppDatabase.kt
+++ app/app/src/main/java/iut/fauryollivier/snoozespot/room/AppDatabase.kt
@@ -1,17 +1,40 @@
 package iut.fauryollivier.snoozespot.room
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import iut.fauryollivier.snoozespot.api.generated.model.PostDTO
+import iut.fauryollivier.snoozespot.room.dao.PostDao
 import iut.fauryollivier.snoozespot.room.dao.SpotDao
+import org.openapitools.client.models.room.PostDTORoomModel
 import org.openapitools.client.models.room.SpotDTORoomModel
 
-@Database(entities = [SpotDTORoomModel::class], version = 1)
+@Database(entities = [SpotDTORoomModel::class, PostDTORoomModel::class], version = 1)
 @TypeConverters(RoomJsonConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun SpotDao(): SpotDao
+    abstract fun PostDao(): PostDao
+
+    suspend fun savePosts(posts: List<PostDTO>) {
+
+        val toRemove = PostDao().count() - (20 - posts.count())
+        PostDao().deleteLastN(toRemove)
+
+        posts.forEach { dto ->
+            PostDao().insert(PostDTORoomModel(
+                dto.id,
+                dto.id,
+                dto.user,
+                dto.content,
+                dto.likeCount,
+                dto.likedByUser,
+                dto.createdAt,
+            ))
+        }
+    }
 }
 
 object DatabaseBuilder {
---
+++ app/app/src/main/java/iut/fauryollivier/snoozespot/room/dao/PostDao.kt
@@ -0,0 +1,38 @@
+package iut.fauryollivier.snoozespot.room.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import org.openapitools.client.models.room.PostDTORoomModel
+
+@Dao
+interface PostDao {
+    @Query("SELECT * FROM PostDTO")
+    suspend fun getAll(): List<PostDTORoomModel>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(post: PostDTORoomModel)
+
+    @Delete
+    suspend fun delete(post: PostDTORoomModel)
+
+    @Query("SELECT EXISTS(SELECT 1 FROM PostDTO WHERE id = :postId)")
+    suspend fun exist(postId: Int): Boolean
+
+    @Query("DELETE FROM postdto WHERE id = :postId")
+    suspend fun deleteById(postId: Int)
+
+    @Query("SELECT COUNT(*) FROM PostDTO")
+    suspend fun count(): Int
+
+    @Query("""
+        DELETE FROM PostDTO 
+        WHERE id IN (
+            SELECT id FROM PostDTO ORDER BY id DESC LIMIT :count
+        )
+    """)
+    suspend fun deleteLastN(count: Int)
+
+}
