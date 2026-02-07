da0646e8c97b2bef419f6fbafdd895bacce4a590
Jonathan <70845195+JonathanOll@users.noreply.github.com>
2026-02-07 09:08:26 +0100

    Fix profile pic and loader (#57)
    * fix mdp et username inversé
    
    * Loaders & fix expandable profile pic

--- app/app/src/main/java/iut/fauryollivier/snoozespot/app/components/UserProfilePicture.kt
+++ app/app/src/main/java/iut/fauryollivier/snoozespot/app/components/UserProfilePicture.kt
@@ -1,17 +1,10 @@
 package iut.fauryollivier.snoozespot.app.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import iut.fauryollivier.snoozespot.R
 import iut.fauryollivier.snoozespot.api.data.NetworkDataSource
@@ -26,18 +19,18 @@ fun UserProfilePicture(
 ) {
     if (user.profilePicture != null)
         if (expandable) {
-            AsyncImage(
-                NetworkDataSource.BASE_URL + user.profilePicture.path,
-                stringResource(R.string.profile_picture),
-                modifier = modifier,
-                error = painterResource(R.drawable.default_profile_picture)
-            )
-        } else {
             ExpandableImage(
                 imageUri = NetworkDataSource.BASE_URL + user.profilePicture.path,
                 contentDescription = "Avatar de ${user.username}",
                 modifier = modifier,
                 defaultClickable = onClickOnProfilePic == null
+            )
+        } else {
+            AsyncImage(
+                NetworkDataSource.BASE_URL + user.profilePicture.path,
+                stringResource(R.string.profile_picture),
+                modifier = modifier,
+                error = painterResource(R.drawable.default_profile_picture)
             )
         }
 
